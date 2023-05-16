08177121f2050b0a8c92dc3c2a579311618d98f9
Carlo Supina <cdsupina@gmail.com>
2023-05-16 03:21:01 -0500

    fix timer and add neopixel strips

--- arcade/arcade.ino
+++ arcade/arcade.ino
@@ -1,4 +1,8 @@
-#include <TimerOne.h>
+#include <arduino-timer.h>
+#include <Adafruit_NeoPixel.h>
+
+auto timer_1 = timer_create_default();
+//auto timer_2 = timer_create_default();
 
 // available patterns for leds
 enum LEDPattern {
@@ -16,11 +20,20 @@ struct LEDButton {
 };
 
 // initialize led buttons
-LEDButton p1_green = {2, off, 0, 5};
-LEDButton p1_purple = {3, off, 0, 5};
-LEDButton p2_green = {4, off, 0, 5};
-LEDButton p2_purple = {5, off, 0, 5};
-LEDButton red = {6, off, 0, 5};
+LEDButton p1_green = {11, off, 0, 5};
+LEDButton p1_purple = {13, off, 0, 5};
+LEDButton p2_green = {9, off, 0, 5};
+LEDButton p2_purple = {10, off, 0, 5};
+LEDButton red = {5, off, 0, 5};
+int right_led_strip = 3;
+int right_idx = 0;
+int right_update_flag = false;
+Adafruit_NeoPixel right_strip(30, right_led_strip, NEO_GRB + NEO_KHZ800);
+int left_led_strip = 6;
+int left_idx = 0;
+int left_update_flag = false;
+Adafruit_NeoPixel left_strip(30, left_led_strip, NEO_GRB + NEO_KHZ800);
+
 
 // create buffer with one byte for each button
 const byte numBytes = 5;
@@ -33,20 +46,62 @@ boolean newData = false;
 int timer_period = 30000;
 
 void setup() {
+  right_strip.begin();
+  right_strip.show();
+  right_strip.setBrightness(50);
+  left_strip.begin();
+  left_strip.show();
+  left_strip.setBrightness(50);
+
   //setup timer
-  Timer1.initialize(timer_period);
-  Timer1.attachInterrupt(updatePatterns);
+  timer_1.every(30, updatePatterns);
+  timer_1.every(100, updateColorWipe);
 
   // setup serial
   Serial.begin(115200);
 }
 
 void loop() {
+  timer_1.tick();
+  //timer_2.tick();
+
   // receive bytes from game
   recvWithEndMarker();
 
   // update LED patterns
   updateLEDs();
+
+  // update right neopixel strip
+  if (right_update_flag) {
+    if (right_idx < 30) {
+      right_strip.setPixelColor(right_idx, right_strip.Color(0,255,0));         //  Set pixel's color (in RAM)
+      right_strip.show();
+      right_idx ++;
+    }else if (right_idx >= 30 && right_idx < 60) {
+      right_strip.setPixelColor(right_idx - 30, right_strip.Color(255,0,255));         //  Set pixel's color (in RAM)
+      right_strip.show();
+      right_idx ++;
+    } else {
+      right_idx = 0;
+    }
+    right_update_flag = false;
+  }
+
+  // update left neopixel strip
+  if (left_update_flag) {
+    if (left_idx < 30) {
+      left_strip.setPixelColor(left_idx, left_strip.Color(0,255,0));         //  Set pixel's color (in RAM)
+      left_strip.show();
+      left_idx ++;
+    }else if (left_idx >= 30 && left_idx < 60) {
+      left_strip.setPixelColor(left_idx - 30, left_strip.Color(255,0,255));         //  Set pixel's color (in RAM)
+      left_strip.show();
+      left_idx ++;
+    } else {
+      left_idx = 0;
+    }
+    left_update_flag = false;
+  }
 }
 
 void recvWithEndMarker() {
@@ -113,10 +168,20 @@ void updatePattern(LEDButton &led_button) {
 }
 
 // update all led button patterns
-void updatePatterns() {
+void updatePatterns(void *) {
   updatePattern(p1_green);
   updatePattern(p1_purple);
   updatePattern(p2_green);
   updatePattern(p2_purple);
   updatePattern(red);
+
+  return true;
+}
+
+void updateColorWipe(void *) {
+  //Serial.println(right_idx);
+  right_update_flag = true;
+  left_update_flag = true;
+
+  return true;
 }
